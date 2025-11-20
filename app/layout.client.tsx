aeec829434c162a5b0f79a4686fdc6353233d7d2
burakozcan <burakozcan@burakozcan-MacBook-Pro.local>
2025-11-20 18:01:17 +0300

    edit body

--- app/layout.client.tsx
+++ app/layout.client.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import { type ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { cn } from "fumadocs-ui/utils/cn";
 import Image from "next/image";
 import Link from "next/link";
 
-export function Body({
-  children,
-}: {
-  children: ReactNode;
-}): React.ReactElement {
-  const mode = useMode();
 
-  return (
-    <body className={cn(mode, "relative flex min-h-screen flex-col")}>
-      {children}
-    </body>
-  );
-}
-
-export function useMode(): string | undefined {
-  const { slug } = useParams();
-  return Array.isArray(slug) && slug.length > 0 ? slug[0] : undefined;
-}
 
 export function FumadocsIcon(props: React.SVGProps<SVGSVGElement>) {
   const [isDark, setIsDark] = useState(false);
